d48e47262b5e942942948c90798d729a205e7535
Filip <filip.kramar4637@gmail.com>
2023-06-17 10:17:31 +0200

    Initial commit

---
+++ src/main/java/hr/kingict/akademija2023/SpringBootAkademija2023/SpringBootAkademija2023Application.java
@@ -0,0 +1,13 @@
+package hr.kingict.akademija2023.SpringBootAkademija2023;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAkademija2023Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAkademija2023Application.class, args);
+	}
+
+}
